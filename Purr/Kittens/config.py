c09f2a1f2bb9eb376b954f0d226390fc0fc63510
Oleg Smirnov <osmirnov@gmail.com>
2012-06-13 14:55:26 +0000

    Small modifications to DualConfigParser -- added a has_section() method

--- Purr/Kittens/config.py
+++ Purr/Kittens/config.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 #
-#% $Id$ 
+#% $Id$
 #
 #
 # Copyright (C) 2002-2007
-# The MeqTree Foundation & 
+# The MeqTree Foundation &
 # ASTRON (Netherlands Foundation for Research in Astronomy)
 # P.O.Box 2, 7990 AA Dwingeloo, The Netherlands
 #
@@ -22,7 +22,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>,
-# or write to the Free Software Foundation, Inc., 
+# or write to the Free Software Foundation, Inc.,
 # 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
 
@@ -31,7 +31,7 @@ import os
 import os.path
 import sys
 
-_default_system_paths = [ 
+_default_system_paths = [
   "/usr/local/Timba",
   "/usr/Timba/",
   "/etc/" ];
@@ -42,38 +42,45 @@ class DualConfigParser (object):
   """A dual config parser taking into account both system-wide files
   and user defaults. Any changes are stored in the user defaults."""
   def __init__ (self,filename="timba.conf",
-		system_paths=_default_system_paths,
-		user_path=_default_user_path):
+                system_paths=_default_system_paths,
+                user_path=_default_user_path):
     self.syscp = ConfigParser();
     system_paths = [ os.path.join(path,filename) for path in system_paths ];
     self.syscp.read(system_paths);
     self.usercp = ConfigParser();
     self._user_file = os.path.join(user_path,"."+filename);
     self.usercp.read([self._user_file]);
-    
+
   def add_section(self,section):
     if not self.syscp.has_section(section):
       self.syscp.add_section(section);
     if not self.usercp.has_section(section):
       self.usercp.add_section(section);
-      
-  def _get (self,method,section,option,default=None):
+
+  def has_section (self,section):
+    return self.syscp.has_section(section) or self.usercp.has_section(section);
+
+  def _get (self,method,section,option,default=None,init=False,save=False):
     section = section or self.defsection;
     # try user defaults
     try:
       return getattr(self.usercp,method)(section,option);
     except (NoSectionError,NoOptionError,ValueError):
-      error = sys.exc_info()[1]; 
+      error = sys.exc_info()[1];
     # try systemwide
     try:
       return getattr(self.syscp,method)(section,option);
     except (NoSectionError,NoOptionError,ValueError):
       if default is not None:
         self.syscp.set(section,option,str(default));
+        if init or save:
+          self.usercp.set(section,option,str(default));
+          if save:
+            self.usercp.write(file(self._user_file,"w"));
         return default;
-      # no default, so re-raise the error 
+      # no default, so re-raise the error
       raise error;
-  
+
   def set (self,section,option,value,save=True):
     value = str(value);
     # try to get option first, and do nothing if no change
@@ -89,15 +96,15 @@ class DualConfigParser (object):
     self.usercp.set(section,option,value);
     if save:
       self.usercp.write(file(self._user_file,"w"));
-      
+
   def has_option (self,section,option):
     return self.syscp.has_option(section,option) or \
       self.usercp.has_option(section,option);
-      
+
   def get (self,section,option,default=None):
     return self._get('get',option,default,section);
-      
-  
+
+
 class SectionParser (object):
   """A section parser is basically a ConfigParser with a default section name.""";
   def __init__ (self,parser,section):
@@ -108,20 +115,20 @@ class SectionParser (object):
 
   def has_option (self,option,section=None):
     return self.parser.has_option(section or self.section,option);
-  def get (self,option,default=None,section=None):
-    return self.parser._get('get',section or self.section,option,default);
-  def getint (self,option,default=None,section=None):
-    return self.parser._get('getint',section or self.section,option,default);
-  def getfloat (self,option,default=None,section=None):
-    return self.parser._get('getfloat',section or self.section,option,default);
-  def getbool (self,option,default=None,section=None):
-    return self.parser._get('getboolean',section or self.section,option,default);
+  def get (self,option,default=None,section=None,init=False,save=False):
+    return self.parser._get('get',section or self.section,option,default,init=init,save=save);
+  def getint (self,option,default=None,section=None,init=False,save=False):
+    return self.parser._get('getint',section or self.section,option,default,init=init,save=save);
+  def getfloat (self,option,default=None,section=None,init=False,save=False):
+    return self.parser._get('getfloat',section or self.section,option,default,init=init,save=save);
+  def getbool (self,option,default=None,section=None,init=False,save=False):
+    return self.parser._get('getboolean',section or self.section,option,default,init=init,save=save);
   def set (self,option,value,section=None,save=True):
     return self.parser.set(section or self.section,option,value,save=save);
-      
-Config = DualConfigParser(); 
+
+Config = DualConfigParser();
 _section_parsers = {};
-      
+
 def section (name):
   global _section_parsers;
   return _section_parsers.setdefault(name,SectionParser(Config,name));
