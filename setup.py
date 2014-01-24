f3c522ab70cf26feb3aa803c25d547da16522553
Gijs Molenaar <gijs@pythonic.nl>
2014-01-24 12:33:06 +0200

    also install icons

--- setup.py
+++ setup.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python
 
+import os
 from distutils.core import setup
+from distutils.command.install import INSTALL_SCHEMES
+
+data_files = []
+root_dir = os.path.dirname(__file__)
+if root_dir != '':
+    os.chdir(root_dir)
+
+# collect a list of all non python files
+for dirpath, dirnames, filenames in os.walk('Purr'):
+    dirnames[:] = [d for d in dirnames if not d.startswith('.') and d != '__pycache__']
+    if filenames and '__init__.py' not in filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+# Tell distutils not to put the data_files in platform-specific installation
+# locations. See here for an explanation:
+# http://groups.google.com/group/comp.lang.python/browse_thread/thread/35ec7b2fed36eaec/2105ee4d9e8042cb
+for scheme in INSTALL_SCHEMES.values():
+        scheme['data'] = scheme['purelib']
 
 setup(name='purr',
       version='1.3.0',
@@ -9,6 +28,7 @@ setup(name='purr',
       author_email='Oleg Smirnov <osmirnov@gmail.com>',
       url='https://github.com/ska-sa/purr',
       packages=['Purr'],
-      requires=['kittens'],
+      requires=['kittens', 'PyQt4'],
       scripts=['Purr/purr.py', 'Purr/purr'],
+      data_files=data_files,
      )
